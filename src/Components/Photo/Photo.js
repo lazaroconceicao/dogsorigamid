a75cdff0282ecf064302fc82ad40ab980c1d0947
Jose Lazaro <joselazaroc@gmail.com>
2021-10-03 09:39:07 -0300

    CommentsForm

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Photo = () => {
-  return <div></div>;
+  return <div>teste</div>;
 };
 
 export default Photo;
--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -1,31 +1,39 @@
 import React from 'react';
-import { COMMENT_POST } from '../../api/Api';
-import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import Error from '../Helper/Error';
+import { COMMENT_POST } from '../../api/Api';
+import styles from './PhotoCommentsForm.module.css';
 
-const PhotoCommentsForm = (id) => {
+const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
   const { request, error } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    await request(url, options);
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComments('');
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <from className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        ClassName={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button ClassName={styles.button}>
         <Enviar />
       </button>
-    </form>
+      <Error error={error} />
+    </from>
   );
 };
 
