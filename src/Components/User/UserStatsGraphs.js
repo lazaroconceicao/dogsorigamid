9c26f5c223e0b5975759ef44d25f6c2a18b66a06
Jose Lazaro <joselazaroc@gmail.com>
2021-10-21 20:18:30 -0300

    Victory Library on Stats Page

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
     setTotal(
       data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
     );
+    setGraph(graphData);
     console.log(data);
   }, [data]);
 
@@ -16,6 +24,9 @@ const UserStatsGraphs = ({ data }) => {
     <section className={`${styles.graph} animeLeft`}>
       <div className={styles.total}>
         <p>Acessos: {total}</p>
+      </div>
+      <div>
+        <VictoryPie data={[{ graph }]} />
       </div>
     </section>
   );
