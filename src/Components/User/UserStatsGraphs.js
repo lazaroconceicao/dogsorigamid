27f788436c38712ce0891d82d8ad9ea9643c35bb
Jose Lazaro <joselazaroc@gmail.com>
2021-10-21 20:25:17 -0300

    Styles VictoryPie and Creat VictoryBar

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -26,7 +26,27 @@ const UserStatsGraphs = ({ data }) => {
         <p>Acessos: {total}</p>
       </div>
       <div>
-        <VictoryPie data={[{ graph }]} />
+        <VictoryPie
+          data={[{ graph }]}
+          innerRadius={50}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#ff',
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333',
+            },
+          }}
+        />
+      </div>
+      <div>
+        <VictoryChart>
+          <VictoryBar alignment="start" data={graph}></VictoryBar>
+        </VictoryChart>
       </div>
     </section>
   );
